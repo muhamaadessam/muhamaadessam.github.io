106a28c828caf1e4f0f93c2a8fc5e1e204cd3a27
muhammadessam <muhammad159e@gmail.com>
2025-06-02 20:01:11 +0300

    Update Portfolio 02/06 08:01 PM

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3764065641"
+    serviceWorkerVersion: "421329611"
   }
 });
